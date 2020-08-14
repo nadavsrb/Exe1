bc45f411e4367a67ceae3b7403baebc7979a1a0b
root <root@DESKTOP-HKCE1KB.localdomain>
2020-08-14 09:52:21 +0300

    Adding Matrix struct in Matrix.c

---
+++ Matrix.c
@@ -0,0 +1,5 @@
+struct Matrix {
+     int numRows; // number of rows
+     int numCols; // number of columns
+     double** data; // a pointer to an array of numCols rows that each points to numCols double; 
+};
