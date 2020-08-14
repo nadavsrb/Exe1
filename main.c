48fdddf28301fa679eb7ce0dd5048293fc792464
root <root@DESKTOP-HKCE1KB.localdomain>
2020-08-14 11:36:34 +0300

    Adding to main.c notes & check for copy_matrix

--- main.c
+++ main.c
@@ -11,12 +11,26 @@ int main() {
         return 1;
     }
 
+    //creates matrix
     matrix_create(matrix, 5, 5);
+    printf("Created matrix:\n");
+    print_matrix(*matrix);
 
-    print_matrix((*matrix));
+    //copy matrix
+    PMatrix *copyMatrix;
+    matrix_copy(copyMatrix, *matrix);
+    printf("copied matrix:\n");
+    print_matrix(*copyMatrix);
+
+
     return 0;
 }
 
+/**
+ * @brief get matrix and print it.
+ * 
+ * @param matrix the matrix to print.
+ */
 void print_matrix(PMatrix matrix){
     for (int i = 0; i < (matrix->numRows); i++){
         for (int j = 0; j < (matrix->numCols); j++){
